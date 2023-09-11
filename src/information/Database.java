78a66525a5cce4f817ec393d30b99958b0ffef83
maztas-tech <mata0001@stud.kea.dk>
2023-09-11 12:34:39 +0200

    Added fields constructor, get and set in superhero class.

--- src/information/Database.java
+++ src/information/Database.java
@@ -1,4 +1,5 @@
 package information;
 
 public class Database {
+
 }
--- src/information/Superhero.java
+++ src/information/Superhero.java
@@ -1,4 +1,63 @@
 package information;
-
 public class Superhero {
+    //Fields
+    private String name;
+    private String realName;
+    final private String superPower;
+    final private int yearCreated;
+    private int strength;
+
+    //Constructor - with superhero name
+    public Superhero(String name, String realName, String superPower,
+                     int yearCreated, int strength){
+        this.name = name;
+        this.realName = realName;
+        this.superPower = superPower;
+        this.yearCreated = yearCreated;
+        this.strength = strength;
+    }
+
+    //Constructor - without superhero name
+    public Superhero(String realName, String superPower, int yearCreated,
+                     int strength){
+        this.realName = realName;
+        this.superPower = superPower;
+        this.yearCreated = yearCreated;
+        this.strength = strength;
+    }
+
+    //Getter
+    public String getName(){
+        return name;
+    }
+    public String getRealName(){
+        return realName;
+    }
+
+    public String getSuperPower(){
+        return superPower;
+    }
+    public int getYearCreated(){
+        return yearCreated;
+    }
+
+    public int getStrength(){
+        return strength;
+    }
+
+    //Setter
+    public void setRealName(){
+        this.realName = realName;
+    }
+    public void setName(){
+        this.name = name;
+    }
+    public void setStrength(){
+        this.strength = strength;
+    }
+
+
+
+
+
 }
