a1faa2d345ed3d37b252bee42ff45da013637012
maztas-tech <mata0001@stud.kea.dk>
2023-09-11 13:09:09 +0200

    Added constructor and 2 methods. 1 method takes name parameter 2 method does not take name parameter

--- src/information/Database.java
+++ src/information/Database.java
@@ -1,5 +1,23 @@
 package information;
 
 public class Database {
+    //Fields
+    private Superhero[] superheroes;
+    int count = 0;
 
+    //Constructor
+    public Database(){
+        this.superheroes = new Superhero[6];
+    }
+    //First method - with superhero name.
+    public void addSuperheroWithName(String name, String realName, String superPower,
+                        int yearCreated, int strength){
+        superheroes[count++] = new Superhero(name, realName, superPower, yearCreated, strength);
+    }
+
+    //Second method without superhero name.
+    public void addSuperheroWithoutName(String realName, String superPower,
+                                        int yearCreated, int strength){
+        superheroes[count++] = new Superhero(realName, superPower, yearCreated, strength);
+    }
 }
