acf0ca24f43cfe71cef6078a90c6a92b92e681c9
maztas-tech <mata0001@stud.kea.dk>
2023-09-11 14:47:32 +0200

    Added it a forloop in main

--- src/Main.java
+++ src/Main.java
@@ -12,59 +12,61 @@ public class Main {
         System.out.println("1. Opret superhelt ");
         System.out.println("9. Afslut ");
         valg = readInt(keyboard);
+        for (int i = 0; i <= 6; i++) {
+            if (valg == 1) {
+                String name;
+                String realName;
+                String superPower;
+                int yearCreated;
+                int strength;
+                String isHuman;
+                System.out.print("Har du et identitet? Y/N: ");
+                proceed = keyboard.nextLine().charAt(0);
+                if (proceed == 'Y' || proceed == 'y') {
+                    System.out.print("Dit identitet: ");
+                    name = keyboard.nextLine();
+                    System.out.print("Sande identitet: ");
+                    realName = keyboard.nextLine();
+                    System.out.print("Dine superkræfter: ");
+                    superPower = keyboard.nextLine();
+                    System.out.print("Er du et menneske? Y/N: ");
+                    isHuman = keyboard.nextLine();
 
-        if (valg == 1) {
-            String name;
-            String realName;
-            String superPower;
-            int yearCreated;
-            int strength;
-            String isHuman;
-            System.out.print("Har du et identitet? Y/N: ");
-            proceed = keyboard.nextLine().charAt(0);
-            if (proceed == 'Y' || proceed == 'y') {
-                System.out.print("Dit identitet: ");
-                name = keyboard.nextLine();
-                System.out.print("Sande identitet: ");
-                realName = keyboard.nextLine();
-                System.out.print("Dine superkræfter: ");
-                superPower = keyboard.nextLine();
-                System.out.print("Er du et menneske? Y/N: ");
-                isHuman = keyboard.nextLine();
+                    System.out.print("Årstal du blev født: ");
+                    yearCreated = readInt(keyboard);
+                    System.out.print("Dine stats: ");
+                    strength = readInt(keyboard);
 
-                System.out.print("Årstal du blev født: ");
-                yearCreated = readInt(keyboard);
-                System.out.print("Dine stats: ");
-                strength = readInt(keyboard);
+                    superhero.addSuperheroWithName(name, realName, superPower,
+                            isHuman, yearCreated,  strength);
 
-                superhero.addSuperheroWithName(name, realName, superPower,
-                        isHuman, yearCreated,  strength);
+                    System.out.println(superhero);
+                } else if (proceed == 'N' || proceed == 'n') {
+                    System.out.print("Sande identitet: ");
+                    realName = keyboard.nextLine();
+                    System.out.print("Dine superkræfter: ");
+                    superPower = keyboard.nextLine();
+                    System.out.println("Er du et menneske: ");
+                    isHuman = keyboard.nextLine();
 
-                System.out.println(superhero);
-            } else if (proceed == 'N' || proceed == 'n') {
-                System.out.print("Sande identitet: ");
-                realName = keyboard.nextLine();
-                System.out.print("Dine superkræfter: ");
-                superPower = keyboard.nextLine();
-                System.out.println("Er du et menneske: ");
-                isHuman = keyboard.nextLine();
+                    System.out.print("Årstal du blev født: ");
+                    yearCreated = readInt(keyboard);
+                    System.out.print("Dine stats: ");
+                    strength = readInt(keyboard);
 
-                System.out.print("Årstal du blev født: ");
-                yearCreated = readInt(keyboard);
+                    superhero.addSuperheroWithoutName(realName, superPower,
+                            isHuman, yearCreated,  strength);
 
-                System.out.print("Dine stats: ");
-                strength = readInt(keyboard);
+                    System.out.println(superhero);
+                }
 
-                superhero.addSuperheroWithoutName(realName, superPower,
-                        isHuman, yearCreated,  strength);
-
-                System.out.println(superhero);
+            } else if (valg == 9){
+                System.out.println("No options have been picked");
+                System.exit(0);
             }
 
-        } else if (valg == 9){
-            System.out.println("No options have been picked");
-            System.exit(0);
         }
+
 
     }
     // Avoids scanner bugs for boolean and ints.
