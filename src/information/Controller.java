6d560c57c00623a58c8d97bb3f4b3d7be5175d1f
maztas-tech <mata0001@stud.kea.dk>
2023-09-12 19:32:38 +0200

    Added a few controller methods.

--- src/information/Controller.java
+++ src/information/Controller.java
@@ -1,4 +1,22 @@
 package information;
 
-public class Controller {
+public class Controller
+{
+    private Database db;
+
+    public Controller(Database db){
+        this.db = db;
+    }
+
+    public void addSuperheroWithName(String name, String realName, String superPower, String isHuman,
+                                     int yearCreated, int strength){
+        db.addSuperheroWithName(name, realName, superPower, isHuman, yearCreated, strength);
+    }
+
+    public void addSuperheroWithoutName(String realName, String superPower, String isHuman,
+                                        int yearCreated, int strength){
+        db.addSuperheroWithoutName(realName, superPower, isHuman, yearCreated, strength);
+    }
+
+
 }
