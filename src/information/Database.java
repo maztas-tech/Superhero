3887d6d294fc45c3bba4b56ddff0f544587cf428
maztas-tech <mata0001@stud.kea.dk>
2023-09-20 09:43:42 +0200

    Fixed typos and added {} on line 49 - 50, database class.

--- src/information/Database.java
+++ src/information/Database.java
@@ -1,23 +1,22 @@
 package information;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Database {
     private ArrayList<Superhero> superheroes = new ArrayList<Superhero>();
 
-    //Add superhero - Crud
+    //Add a superhero - Crud
     public void addSuperhero(String secretIdentity, String trueIdentity, String superpower,
                              int yearCreated, String isHuman, int strength){
         superheroes.add(new Superhero(secretIdentity, trueIdentity, superpower, yearCreated,
                 isHuman, strength));
     }
 
-    //Recieve all superheroes - cRud
+    //Receive all superheroes - cRud
     public ArrayList<Superhero> recieveSuperheroes(){
         return superheroes;
     }
 
-    //Recieve a specific superhero - cRud
+    //Receive a specific superhero - cRud
     public Superhero searchSuperhero(String name){
         //Foreach loop that searches if a superhero matches the search criteria
         for (Superhero superhero: superheroes) {
@@ -38,31 +37,7 @@ public class Database {
             }
         }
     }
-    /*public void editSuperhero(String name, Scanner input){
-        for (Superhero superhero: superheroes) {
-            if (superhero.getSecretIdentity().contains(name)){
-                System.out.print("Please type in the new secret identity: ");
-                String newSecretIdentity;
-                newSecretIdentity = input.nextLine();
-                System.out.println("New secret identity has been implemented!");
-
-                System.out.print("Please type in the new real identity: ");
-                String newTrueIdentity;
-                newTrueIdentity = input.nextLine();
-                System.out.println("New true identity has been implemented!");
-
-                System.out.print("Please type in the new strength value: ");
-                int newStrength;
-                newStrength = input.nextInt();
-                System.out.println("New strength has been implemented!");
-
-                superhero.setSecretIdentity(newSecretIdentity);
-                superhero.setTrueIdentity(newTrueIdentity);
-                superhero.setStrength(newStrength);
-            }
-        }
-    }*/
-    //Remove supperhero - cruD
+    //Remove superhero - cruD
     public void removeSuperhero(String secretIdentity){
         Superhero found = null;
         for (Superhero superhero: superheroes) {
@@ -70,6 +45,8 @@ public class Database {
                 found = superhero;
             }
         }
-        if (found != null) superheroes.remove(found);
+        if (found != null){
+            superheroes.remove(found);
+        }
     }
 }
